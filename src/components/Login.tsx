dd5f368cdc998e83468399be8d25ad2eae973ba9
MohdFaisalBidda <biddafaisal@gmail.com>
2024-04-05 02:00:31 +0530

    Feat: login and navbar and basic setup.

---
+++ src/components/Login.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="my-10 flex w-full items-center justify-center">
+      <div className="h-full max-h-[691px] w-full max-w-[576px] rounded-[20px] border border-[#C1C1C1] p-14">
+        <h1 className="text-center text-[32px] font-semibold">
+          Create your account
+        </h1>
+        <div className="my-4 flex w-auto max-w-[456px] flex-col gap-y-1">
+          <label htmlFor="" className="text-base font-normal">
+            Name
+          </label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="rounded-md border border-[#C1C1C1] p-3"
+          />
+        </div>
+        <div className="my-6 flex w-auto max-w-[456px] flex-col gap-y-1">
+          <label htmlFor="" className="text-base font-normal">
+            Email
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="rounded-md border border-[#C1C1C1] p-3"
+          />
+        </div>
+        <div className="my-4 flex w-auto max-w-[456px] flex-col gap-y-1">
+          <label htmlFor="" className="text-base font-normal">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="rounded-md border border-[#C1C1C1] p-3"
+          />
+        </div>
+        <div className="mt-10 w-auto max-w-[456px] rounded-md bg-black p-4 text-center text-white">
+          <button className="text-center text-base font-medium capitalize">
+            Create account
+          </button>
+        </div>
+        <div className="my-14 flex justify-center gap-x-2 text-center">
+          <p className="text-base font-normal text-[#333333]">
+            Have an Account?
+          </p>
+          <span className="text-base font-normal capitalize">Login</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { LuShoppingCart } from "react-icons/lu";
+import { FiSearch } from "react-icons/fi";
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+} from "react-icons/md";
+
+function Navbar() {
+  return (
+    <>
+      <div className="flex h-[100px] w-full flex-col justify-between bg-white py-4 lg:px-16">
+        <div className="flow-row flex h-9 px-16 lg:px-0">
+          <ul className="flex w-full items-center justify-end gap-x-10">
+            <li className="text-xs font-normal">Help</li>
+            <li className="text-xs font-normal">Orders & Returns</li>
+            <li className="text-xs font-normal">Hi,John</li>
+          </ul>
+        </div>
+        <div className="flex h-full items-center justify-between px-4 pt-4 lg:px-0">
+          <h1 className="text-[32px] font-bold capitalize">Ecommerce</h1>
+          <ul className="hidden gap-x-10 lg:flex">
+            <li className="text-base font-semibold ">Categories</li>
+            <li className="text-base font-semibold ">Sale</li>
+            <li className="text-base font-semibold ">Clearance</li>
+            <li className="text-base font-semibold ">New stock</li>
+            <li className="text-base font-semibold ">Trending</li>
+          </ul>
+          <div className="flex gap-x-10">
+            <div className="h-[32px] w-[32px]">
+              <FiSearch color="#333333" className="h-[19.5px] w-[21.75px]" />
+            </div>
+            <div className="h-[32px] w-[32px]">
+              <LuShoppingCart
+                color="#333333"
+                className="h-[19.5px] w-[21.75px]"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex h-9 items-center justify-center gap-x-6 bg-[#F4F4F4] lg:px-16">
+        <MdOutlineKeyboardArrowLeft className="h-5 w-5" />
+        <p className="text-sm font-medium">Get 10% off on business sign up</p>
+        <MdOutlineKeyboardArrowRight className="h-5 w-5" />
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
